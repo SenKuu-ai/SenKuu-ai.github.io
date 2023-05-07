90a80c844f655c60fce4acf3d35fc5a32b2852ca
senkuu <1274346563@qq.com>
2023-05-07 15:40:20 +0800

    Site updated: 2023-05-07 15:40:20

--- jxnd/2023-4-29-五一武汉行/photo-wall.js
+++ jxnd/2023-4-29-五一武汉行/photo-wall.js
@@ -6,50 +6,46 @@ var client = new OSS({
   bucket: 'awaken-age'
 });
 
-// Intersection Observer 配置
-var observerOptions = {
-  root: null, // 默认为视口
-  threshold: 0.1 // 当图片进入视口超过 10% 时加载图片
-};
-
 // 获取照片URL列表
 client.list({
   'max-keys': 1000, // 最大返回1000个文件
-  'prefix': '2023-4-29-五一武汉行/' // 指定获取以'test/'开头的文件列表
+  'prefix': 'test/' // 指定获取以'photos/'开头的文件列表
 }).then(function(result) {
   var photos = result.objects; // 获取文件列表
 
-  // 创建 Intersection Observer 实例
-  var observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        var photo = entry.target;
-        var photoUrl = client.signatureUrl(photo.dataset.name, { expires: 3600 }); // 生成URL，有效期为1小时
-        photo.src = photoUrl; // 加载图片
-        observer.unobserve(photo); // 停止观察该图片
-      }
-    });
-  }, observerOptions);
-
   // 生成照片墙
   for (var i = 0; i < photos.length; i++) {
-    var photo = photos[i];
+      var photo = photos[i];
 
-    // 判断文件类型为图片
-    if (photo.size && photo.name.match(/\.jpe?g$|\.png$|\.gif$/i)) {
-      var photoUrl = client.signatureUrl(photo.name, { expires: 3600 }); // 生成URL，有效期为1小时
+      // 判断文件类型为图片
+      if (photo.size && photo.name.match(/\.jpe?g$|\.png$|\.gif$/i)) {
+          // 生成略缩图URL
+          var thumbnailUrl = client.signatureUrl(photo.name, {process: 'image/resize,w_200'});
 
-      // 创建 img 元素
-      var img = document.createElement('img');
-      img.dataset.name = photo.name; // 保存图片文件名
-      img.setAttribute('data-src', photoUrl); // 设置图片 URL 为 data-src 属性
-      img.className = 'lazy-img'; // 添加懒加载图片的类名
+          // 生成原图URL，有效期为1小时
+          var originalUrl = client.signatureUrl(photo.name, {expires: 3600});
 
-      // 将 img 元素添加到照片墙中
-      document.getElementById('photo-wall').appendChild(img);
+          // 生成图片标签
+          var $img = $('<img>').attr('src', thumbnailUrl).attr('data-original', originalUrl);
 
-      // 开始观察图片
-      observer.observe(img);
-    }
-  }
+          // 绑定点击事件
+          $img.on('click', function() {
+              var $this = $(this);
+              var originalUrl = $this.attr('data-original');
+
+              // 显示原图
+              $this.attr('src', originalUrl);
+
+              // 添加下载链接
+              var $downloadLink = $('<a>').attr('href', originalUrl).attr('download', '');
+              $downloadLink[0].click();
+          });
+
+          // 添加到照片墙中
+          $('#photo-wall').append($img);
+      }
+  } 
+
+  // 启用懒加载插件
+  $('#photo-wall').lazyload();
 });
